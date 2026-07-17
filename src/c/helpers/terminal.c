de0612f00380950e90bd4d9e9a9f63f50406d922
itaybenshushan <iybnsn@gmail.com>
2026-07-17 11:11:58 +0300

    improved terminal output system

--- src/c/helpers/terminal.c
+++ src/c/helpers/terminal.c
@@ -1,10 +1,18 @@
 #include "terminal.h"
 #include "vga.h"
+#include "common.h"
 #include <stddef.h>
 #include <stdint.h>
+#include <stdbool.h>
 
-// --- CORE LAYOUT HELPERS ---
-size_t get_row_written_length(size_t y) {
+static size_t terminal_viewport_top;
+static size_t terminal_row;
+static size_t terminal_col;
+static uint8_t terminal_color;
+static uint16_t* terminal_viewport_buffer;
+static uint16_t terminal_buffer[VGA_WIDTH * RECENT_ROWS];
+
+static size_t get_row_written_length(size_t y) {
     for (int x = (int)VGA_WIDTH - 1; x >= 0; x--) {
         size_t index = y * VGA_WIDTH + (size_t)x;
         char c = (char)(terminal_buffer[index] & 0xFF);
@@ -15,19 +23,19 @@ size_t get_row_written_length(size_t y) {
     return 0;
 }
 
-void shift_history_up(void) {
+static void shift_history_up(void) {
     for (size_t y = 1; y < RECENT_ROWS; y++) {
         for (size_t x = 0; x < VGA_WIDTH; x++) {
             terminal_buffer[(y - 1) * VGA_WIDTH + x] = terminal_buffer[y * VGA_WIDTH + x];
-            
-            if (y == RECENT_ROWS - 1) {
-                terminal_buffer[y * VGA_WIDTH + x] = vga_entry(' ', terminal_color);
-            }
         }
+    }
+
+    for (size_t x = 0; x < VGA_WIDTH; x++) {
+        terminal_buffer[(RECENT_ROWS - 1) * VGA_WIDTH + x] = vga_entry(' ', terminal_color);
     }
 }
 
-void terminal_refresh_screen(void) {
+static void terminal_refresh_screen(void) {
     for (size_t y = 0; y < VGA_HEIGHT; y++) {
         for (size_t x = 0; x < VGA_WIDTH; x++) {
             size_t history_index = (terminal_viewport_top + y) * VGA_WIDTH + x;
@@ -37,7 +45,7 @@ void terminal_refresh_screen(void) {
     }
 }
 
-void increase_row(void) {
+static void increase_row(void) {
     terminal_row++;
     
     if (terminal_row >= VGA_HEIGHT) {
@@ -48,31 +56,27 @@ void increase_row(void) {
         shift_history_up();
         terminal_row = RECENT_ROWS - 1;
         terminal_viewport_top = RECENT_ROWS - VGA_HEIGHT;
-    }
-    
-    terminal_refresh_screen();
+    }    
 }
 
-void increase_col(void) {
+static void increase_col(void) {
     if (++terminal_col >= VGA_WIDTH) {
         increase_row();
         terminal_col = 0;
     }
 }
 
-void decrease_row(void) {
+static void decrease_row(void) {
     if (terminal_row > 0) {
         terminal_row--;
         
         if (terminal_row < terminal_viewport_top) {
             terminal_viewport_top = terminal_row;
-        }
-        
-        terminal_refresh_screen();
+        }        
     }
 }
 
-void decrease_col(void) {
+static void decrease_col(void) {
     if (terminal_col > 0) {
         terminal_col--;
     } else {
@@ -83,18 +87,42 @@ void decrease_col(void) {
     }
 }
 
-bool is_coordinate_empty(size_t x, size_t y) {
-    size_t index = y * VGA_WIDTH + x;
-    char c = (char)(terminal_buffer[index] & 0xFF); 
-    return (c == ' ');
-}
-
-bool is_escape_sequence(char c) {
+static bool is_escape_sequence(char c) {
     return c == '\n' || c == '\r' || c == '\t' || c == '\b'; 
 }
 
+static void terminal_flush(void) {
+    terminal_refresh_screen();
+    vga_set_cursor(terminal_col,
+        terminal_row - terminal_viewport_top);
+}
+
+static void terminal_putentryat(char c, uint8_t color, size_t x, size_t y) {
+    const size_t index = y * VGA_WIDTH + x;
+    terminal_buffer[index] = vga_entry(c, color);
+}
+
+static void terminal_putchar_raw(char c) {    
+    if (!is_escape_sequence(c)) {
+        terminal_putentryat(c, terminal_color, terminal_col, terminal_row);
+        increase_col();
+    } else if (c == '\n') {
+        increase_row();
+        terminal_col = 0;
+    }
+}
+
+static void terminal_write(const char* data, size_t size) {
+    for (size_t i = 0; i < size; i++) {
+        terminal_putchar_raw(data[i]);
+    }
+}
+
+
+
+
 // --- PUBLIC TERMINAL INTERFACE ---
-void terminal_initialize (void) {
+void terminal_initialize(void) {
     terminal_viewport_buffer = (uint16_t*)VGA_MEMORY;
     terminal_viewport_top = 0;
     terminal_row = 0;
@@ -109,46 +137,20 @@ void terminal_initialize (void) {
     
     terminal_refresh_screen();
     vga_enable_cursor(14, 15);
-    vga_set_cursor(terminal_col, terminal_row);
+    terminal_flush();
 }
 
-void terminal_setcolor (uint8_t color) {
+void terminal_setcolor(uint8_t color) {
     terminal_color = color;
-}
-
-void terminal_putentryat (char c, uint8_t color, size_t x, size_t y) {
-    const size_t index = y * VGA_WIDTH + x;
-    terminal_buffer[index] = vga_entry(c, color);
-}
-
-void terminal_putchar (char c) {    
-    if (!is_escape_sequence(c)) {
-        terminal_putentryat(c, terminal_color, terminal_col, terminal_row);
-        increase_col();
-    } else if (c == '\n') {
-        increase_row();
-        terminal_col = 0;
-    }
-
-
-    terminal_refresh_screen();
-    vga_set_cursor(terminal_col, terminal_row - terminal_viewport_top);
-}
-
-void terminal_write (const char* data, size_t size) {
-    for (size_t i = 0; i < size; i++) {
-        terminal_putchar(data[i]);
-    }
 }
 
 void terminal_writestring(const char* data) {
     terminal_write(data, strlen(data));
 
-    terminal_refresh_screen();
-    vga_set_cursor(terminal_col, terminal_row - terminal_viewport_top);
+    terminal_flush();
 }
 
-void terminal_readchar() {
+void terminal_readchar(void) {
     uint8_t ch = inb(0x60);
     terminal_putchar(ch);
 }
@@ -160,11 +162,11 @@ void terminal_arrow_handle(char d) {
         case 'l':
             if (terminal_col == 0 && terminal_row > 0) {
                 decrease_row();
-                terminal_col = get_row_written_length(terminal_row);
+                terminal_col = get_row_written_length(terminal_row) - 1;
             } else if (terminal_col > 0) {
                 line_limit = get_row_written_length(terminal_row);
                 if (terminal_col > line_limit) {
-                    terminal_col = line_limit;
+                    terminal_col = line_limit - 1;
                 } else {
                     terminal_col--;
                 }
@@ -188,7 +190,7 @@ void terminal_arrow_handle(char d) {
                 decrease_row();
                 line_limit = get_row_written_length(terminal_row);
                 if (terminal_col > line_limit) {
-                    terminal_col = line_limit;
+                    terminal_col = line_limit - 1;
                 }
             }
             break;
@@ -199,25 +201,29 @@ void terminal_arrow_handle(char d) {
                 if (line_limit > 0) {
                     increase_row();
                     if (terminal_col > line_limit) {
-                        terminal_col = line_limit;
+                        terminal_col = line_limit - 1;
                     }
                 } else {
-                    terminal_col = get_row_written_length(terminal_row);
+                    terminal_col = get_row_written_length(terminal_row) - 1;
                 }
             }
             break;
     }
 
-    vga_set_cursor(terminal_col, terminal_row - terminal_viewport_top);    
+    terminal_flush();
 }
 
-void terminal_delete_last() {
+void terminal_delete_last(void) {
     if (terminal_row == 0 && terminal_col == 0)
         return;
 
     decrease_col();
 
     terminal_putentryat(' ', terminal_color, terminal_col, terminal_row);
-    terminal_refresh_screen();
-    vga_set_cursor(terminal_col, terminal_row - terminal_viewport_top);
+    terminal_flush();
+}
+
+void terminal_putchar(char c) {
+    terminal_putchar_raw(c);
+    terminal_flush();
 }
--- src/c/helpers/terminal.h
+++ src/c/helpers/terminal.h
@@ -1,41 +1,16 @@
-#pragma once
 #ifndef TERMINAL_H
 #define TERMINAL_H
 
-#include <stdbool.h>
-#include <stddef.h>
-#include "vga.h"
-#include "common.h"
+#include <stdint.h>
 
 #define RECENT_ROWS 100
 
-static size_t terminal_viewport_top;
-static size_t terminal_row;
-static size_t terminal_col;
-static uint8_t terminal_color;
-static uint16_t* terminal_viewport_buffer;
-static uint16_t terminal_buffer[VGA_WIDTH * RECENT_ROWS];
-
-
-
-void terminal_initialize ();
-void terminal_setcolor (uint8_t color);
-void terminal_putentryat (char c, uint8_t color, size_t x, size_t y);
-void terminal_putchar (char c);
-void terminal_write (const char* data, size_t size);
+void terminal_initialize(void);
 void terminal_writestring(const char* data);
-void terminal_readchar();
+void terminal_setcolor(uint8_t color);
+void terminal_readchar(void);
+void terminal_putchar(char c);
+void terminal_delete_last(void);
 void terminal_arrow_handle(char d);
-void terminal_delete_last();
-void terminal_refresh_screen();
-
-
-void increase_row();
-void increase_col();
-void decrease_row();
-void decrease_col();
-void shift_history_up();
-bool is_coordinate_empty(size_t x, size_t y);
-bool is_escape_sequence(char c);
 
 #endif //TERMINAL_H
